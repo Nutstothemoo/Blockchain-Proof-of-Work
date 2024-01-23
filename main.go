bfd73c7567e72de6d788e6be78193c90300dd8c7
DUPIN <maximedupin1992@gmail.com>
2024-01-23 10:00:40 +0100

    add proofofWork and RSA signature

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
-	"github.com/gorilla/mux"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -22,6 +26,7 @@ type Block struct {
 	PrevHash     string
 	Difficulty   int
 	Nonce        string
+	Signature    []byte
 }
 
 const difficulty = 1
@@ -32,16 +37,32 @@ var mutex sync.Mutex
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal(err)
+			log.Fatal(err)
+	}
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+			log.Fatal(err)
 	}
 
 	go func() {
-		t := time.Now()
-		genesisBlock := Block{0, t.String(), 0, calculateHash(Block{}), "", difficulty, ""}
-		spew.Dump(genesisBlock)
-		mutex.Lock()
-		Blockchain = append(Blockchain, genesisBlock)
-		mutex.Unlock()
+			t := time.Now()
+			genesisBlock := Block{0, t.String(), 0, "", "", difficulty, "", nil}
+			hash, err := calculateHash(genesisBlock)
+			if err != nil {
+					log.Fatal(err)
+			}
+			genesisBlock.Hash = hash
+
+			genesisBlock.Signature, err = rsa.SignPSS(rand.Reader, privateKey, crypto.SHA256, []byte(genesisBlock.Hash), nil)
+			if err != nil {
+					log.Fatal(err)
+			}
+
+			spew.Dump(genesisBlock)
+			mutex.Lock()
+			Blockchain = append(Blockchain, genesisBlock)
+			mutex.Unlock()
 	}()
 
 	log.Fatal(run())
@@ -89,39 +110,40 @@ func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
 	var newBlock Block
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&newBlock); err != nil {
-		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
-		return
+			respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+			return
 	}
 	defer r.Body.Close()
 
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	if blockIsValid(newBlock, Blockchain[len(Blockchain)-1]) {
-		Blockchain = append(Blockchain, newBlock)
-		respondWithJSON(w, r, http.StatusCreated, newBlock)
+	valid, err := blockIsValid(newBlock, Blockchain[len(Blockchain)-1])
+	if err != nil {
+			respondWithJSON(w, r, http.StatusInternalServerError, "Error validating the block: "+err.Error())
+			return
+	}
+
+	if valid {
+			Blockchain = append(Blockchain, newBlock)
+			respondWithJSON(w, r, http.StatusCreated, newBlock)
 	} else {
-		respondWithJSON(w, r, http.StatusConflict, newBlock)
+			respondWithJSON(w, r, http.StatusConflict, "Block is not valid")
 	}
 }
 
-func blockIsValid(newBlock, oldBlock Block) bool {
-	if oldBlock.Index+1 != newBlock.Index {
-		return false
+func proofOfWork(block Block) string {
+	nonce := 0
+	hash, _ := calculateHash(block)
+	for !isHashValid(hash, difficulty) {
+			nonce++
+			block.Nonce = strconv.Itoa(nonce)
+			hash, _ = calculateHash(block)
 	}
-
-	if oldBlock.Hash != newBlock.PrevHash {
-		return false
-	}
-
-	if calculateHash(newBlock) != newBlock.Hash {
-		return false
-	}
-
-	return true
+	return strconv.Itoa(nonce)
 }
 
-func generateBlock(oldBlock Block, Data int) (Block, error) {
+func generateBlock(oldBlock Block, Data int, privateKey *rsa.PrivateKey) (Block, error) {
 	var newBlock Block
 
 	newBlock.Index = oldBlock.Index + 1
@@ -129,19 +151,57 @@ func generateBlock(oldBlock Block, Data int) (Block, error) {
 	newBlock.Data = Data
 	newBlock.PrevHash = oldBlock.Hash
 	newBlock.Difficulty = difficulty
-	newBlock.Nonce = "TODO" 
 
-	newBlock.Hash = calculateHash(newBlock)
+	newBlock.Nonce = proofOfWork(newBlock)
+	hash, err := calculateHash(newBlock)
+	if err != nil {
+			return Block{}, err
+	}
+	newBlock.Hash = hash
+
+	newBlock.Signature, err = rsa.SignPSS(rand.Reader, privateKey, crypto.SHA256, []byte(newBlock.Hash), nil)
+	if err != nil {
+			return Block{}, err
+	}
 
 	return newBlock, nil
 }
 
-func calculateHash(block Block) string {
+func blockIsValid(newBlock, oldBlock Block, publicKey *rsa.PublicKey) (bool, error) {
+	if oldBlock.Index+1 != newBlock.Index {
+			return false, nil
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+			return false, nil
+	}
+
+	hash, err := calculateHash(newBlock)
+	if err != nil {
+			return false, err
+	}
+
+	if hash != newBlock.Hash {
+			return false, nil
+	}
+
+	err = rsa.VerifyPSS(publicKey, crypto.SHA256, []byte(newBlock.Hash), newBlock.Signature, nil)
+	if err != nil {
+			return false, err
+	}
+
+	return true, nil
+}
+
+func calculateHash(block Block) (string, error) {
 	record := string(rune(block.Index)) + block.Timestamp + string(rune(block.Data)) + block.PrevHash + block.Nonce
 	h := sha256.New()
-	h.Write([]byte(record))
+	_, err := h.Write([]byte(record))
+	if err != nil {
+			return "", err
+	}
 	hashed := h.Sum(nil)
-	return hex.EncodeToString(hashed)
+	return hex.EncodeToString(hashed), nil
 }
 
 func isHashValid(hash string, difficulty int) bool {
